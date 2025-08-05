34e8c9dc66484e0e2d7d4777270172deeaf5fed9
Emirhan Taşdeviren <emirhantasdeviren@gmail.com>
2025-08-05 17:59:41 +0300

    format code

--- src/platform/win32_platform.cpp
+++ src/platform/win32_platform.cpp
@@ -5,25 +5,18 @@
 void *ponePlatformReadFile(char *filename) {
     void *result = 0;
 
-    HANDLE fileHandle = CreateFileA(filename,
-                                    GENERIC_READ,
-                                    FILE_SHARE_READ,
-                                    0,
-                                    OPEN_EXISTING,
-                                    0,
-                                    0);
+    HANDLE fileHandle = CreateFileA(filename, GENERIC_READ, FILE_SHARE_READ, 0,
+                                    OPEN_EXISTING, 0, 0);
     if (fileHandle != INVALID_HANDLE_VALUE) {
         LARGE_INTEGER fileSize;
         if (GetFileSizeEx(fileHandle, &fileSize)) {
             u32 fileSize32 = safeTruncateU32(fileSize.QuadPart);
-            result = VirtualAlloc(0, fileSize32,
-                                  MEM_RESERVE | MEM_COMMIT,
+            result = VirtualAlloc(0, fileSize32, MEM_RESERVE | MEM_COMMIT,
                                   PAGE_READWRITE);
             if (result) {
                 DWORD bytesRead;
                 if (ReadFile(fileHandle, result, fileSize32, &bytesRead, 0) &&
-                    fileSize32 == bytesRead)
-                {
+                    fileSize32 == bytesRead) {
                     // NOTE(emirhan): file read successfully
                 } else {
                     // TODO(emirhan): logging
@@ -47,13 +40,8 @@ void *ponePlatformReadFile(char *filename) {
 b8 ponePlatformWriteFile(char *filename, void *memory, usize memorySize) {
     b8 result = 0;
 
-    HANDLE fileHandle = CreateFileA(filename,
-                                    GENERIC_WRITE,
-                                    0,
-                                    0,
-                                    CREATE_ALWAYS,
-                                    0,
-                                    0);
+    HANDLE fileHandle =
+        CreateFileA(filename, GENERIC_WRITE, 0, 0, CREATE_ALWAYS, 0, 0);
     if (fileHandle != INVALID_HANDLE_VALUE) {
         DWORD bytesWritten;
         if (WriteFile(fileHandle, memory, memorySize, &bytesWritten, 0)) {
