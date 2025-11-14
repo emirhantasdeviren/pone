4d0b23bb923d164e86a0ce5db068d41fc3a6281a
Emirhan Taşdeviren <emirhantasdeviren@gmail.com>
2025-11-14 15:27:04 +0300

    update build commands

--- build.bat
+++ build.bat
@@ -22,5 +22,5 @@ REM clang -Wall -g -O0 -c -I..\include -o imgui_tables.obj ..\src\imgui_tables.c
 REM clang -Wall -g -O0 -c -I..\include -o imgui_widgets.obj ..\src\imgui_widgets.cpp
 REM clang -Wall -g -O0 -c -I..\include -DIMGUI_IMPL_VULKAN_NO_PROTOTYPES -o imgui_impl_vulkan.obj ..\src\imgui_impl_vulkan.cpp
 REM clang -Wall -g -O0 -c -I..\include -o imgui_impl_win32.obj ..\src\imgui_impl_win32.cpp
-clang -Wall -Wno-writable-strings -g -O0 -luser32 -lGdi32 -lWinmm -lSynchronization -o vkguide.exe imgui.obj imgui_demo.obj imgui_draw.obj imgui_tables.obj imgui_widgets.obj imgui_impl_vulkan.obj imgui_impl_win32.obj pone_arena.obj pone_json.obj pone_memory.obj pone_string.obj pone_gltf.obj pone_vulkan.obj pone_truetype.obj pone_math.obj pone_vec2.obj pone_rect.obj pone_atomic.obj pone_work_queue.obj main.obj
+clang -Wall -Wno-writable-strings -g -O0 -luser32 -lGdi32 -lWinmm -lSynchronization -o pone.exe imgui.obj imgui_demo.obj imgui_draw.obj imgui_tables.obj imgui_widgets.obj imgui_impl_vulkan.obj imgui_impl_win32.obj pone_arena.obj pone_json.obj pone_memory.obj pone_string.obj pone_gltf.obj pone_vulkan.obj pone_truetype.obj pone_math.obj pone_vec2.obj pone_rect.obj pone_atomic.obj pone_work_queue.obj main.obj
 popd
--- build_shaders.bat
+++ build_shaders.bat
@@ -3,9 +3,9 @@
 mkdir build
 mkdir build\shaders
 pushd build\shaders
-C:\VulkanSDK\1.4.328.1\Bin\glslc.exe ..\..\shaders\gradient.comp -o gradient.spv
-C:\VulkanSDK\1.4.328.1\Bin\glslc.exe ..\..\shaders\sky.comp -o sky.spv
-C:\VulkanSDK\1.4.328.1\Bin\glslc.exe ..\..\shaders\colored_triangle.vert -o colored_triangle.vert.spv
-C:\VulkanSDK\1.4.328.1\Bin\glslc.exe ..\..\shaders\colored_triangle.frag -o colored_triangle.frag.spv
-C:\VulkanSDK\1.4.328.1\Bin\glslc.exe ..\..\shaders\colored_triangle_mesh.vert -o colored_triangle_mesh.vert.spv
+glslc.exe ..\..\shaders\gradient.comp -o gradient.spv
+glslc.exe ..\..\shaders\sky.comp -o sky.spv
+glslc.exe ..\..\shaders\colored_triangle.vert -o colored_triangle.vert.spv
+glslc.exe ..\..\shaders\colored_triangle.frag -o colored_triangle.frag.spv
+glslc.exe ..\..\shaders\colored_triangle_mesh.vert -o colored_triangle_mesh.vert.spv
 popd
