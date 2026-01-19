a1a1d3ee836a4b75c4b44b00d309bff672e283ec
Emirhan Taşdeviren <emirhantasdeviren@gmail.com>
2026-01-19 23:27:26 +0300

    text_pipeline: define rasterization state

--- src/main.cpp
+++ src/main.cpp
@@ -1721,6 +1721,21 @@ int main(void) {
         .scissorCount = 1,
         .pScissors = 0,
     };
+    VkPipelineRasterizationStateCreateInfo text_pipeline_rasterization_create_info ={
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
+        .pNext = 0,
+        .flags = 0,
+        .depthClampEnable = VK_FALSE,
+        .rasterizerDiscardEnable = VK_FALSE,
+        .polygonMode = VK_POLYGON_MODE_FILL,
+        .cullMode = VK_CULL_MODE_BACK_BIT,
+        .frontFace = VK_FRONT_FACE_CLOCKWISE,
+        .depthBiasEnable = VK_FALSE,
+        .depthBiasConstantFactor = 0.0f,
+        .depthBiasClamp = 0.0f,
+        .depthBiasSlopeFactor = 0.0f,
+        .lineWidth = 1.0f,
+    };
     
     usize frame_index = 0;
     // u64 t0 = pone_platform_get_time();
